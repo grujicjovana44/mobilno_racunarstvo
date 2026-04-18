e27f715066c0d7ec7b4685dee972e319e2690130
unknown <grujicjovana44@gmail.com>
2026-04-18 11:57:50 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'planer-putovanja',
+  webDir: 'www'
+};
+
+export default config;
